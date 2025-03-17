34779ef91173b80b1a0c24d67c9015119652ae7c
Steven Chang <stevenwchang01@gmail.com>
2025-03-17 11:37:17 -0700

    Integrated vector classes into main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main(){
@@ -7,16 +10,11 @@ int main(){
     std::cout << "P3\n" << IMG_WIDTH << ' ' << IMG_HEIGHT << "\n255\n";
 
     for(int j = 0; j < IMG_HEIGHT; j++){
+        std::clog << "\rScanlines remaining: " << (IMG_HEIGHT - j) << ' ' << std::flush;
         for(int i = 0; i < IMG_WIDTH; i++){
-            auto r = double(i)/(IMG_WIDTH-1);
-            auto g = double(j)/(IMG_HEIGHT-1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std:: cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i)/(IMG_WIDTH-1), double(j)/(IMG_HEIGHT-1),0);
+            write_color(std::cout,pixel_color);
         }
     }
+    std::clog << "\rDone.           \n";
 }
